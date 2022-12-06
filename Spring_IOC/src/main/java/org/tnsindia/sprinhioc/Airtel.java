49a04269fa3056dfa1d180eee6c39bffa480e30a
rutuj <rutuj@LAPTOP-HGULBHP5>
2022-12-06 14:06:00 +0530

    Commit

---
+++ Spring_IOC/src/main/java/org/tnsindia/sprinhioc/Airtel.java
@@ -0,0 +1,22 @@
+package org.tnsindia.sprinhioc;
+
+public class Airtel implements Sim{
+
+	public void calling() {
+          System.out.println("Calling using Airtel");		
+	}
+
+	public void data() {
+           System.out.println("Internet using Airtel");		
+	}
+      
+	//constructor
+	
+	public Airtel() {
+		super();
+		System.out.println("Airtel class Constructor");
+	}
+	
+	
+
+}
---
+++ Spring_IOC/src/main/java/org/tnsindia/sprinhioc/Sim.java
@@ -0,0 +1,9 @@
+package org.tnsindia.sprinhioc;
+
+public interface Sim {
+	
+	//abstract method
+	void calling();
+	void data();
+
+}
